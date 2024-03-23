9982641740ca7f0ef635ec692f54d8ca9fb00377
vickapalo <victoria.gyselynck@hotmail.com>
2024-03-23 16:03:59 +0100

    fix heromenu

--- components/HeroMenu.tsx
+++ components/HeroMenu.tsx
@@ -9,7 +9,6 @@ function HeroMenu() {
                 <img src="/assets/images/arbre.jpg" alt="Avatar" className="object-cover w-full h-full" />
                 <div className="font-roboto absolute py-2.5 md:top-6 xl:top-16 bottom-52 left-32  rounded-tl-[15px]  rounded-bl-[120px] rounded-tr-[120px] md:w-[270px] md:h-[243px] lg:w-[437px] lg:h-[343px]    p-10 bg-white/60 text-black">
                     <div className='ml-10'>
-
                         <h1 className='lg:text-6xl md:text-3xl  mt-6'>
                             Éléonore Gyselynck
                         </h1>
@@ -18,10 +17,10 @@ function HeroMenu() {
                         </h2>
                         <div className='text-center'>
 
-                            <h3 className='mt-6 md:text-sm lg:text-base'>
-                                Consultations à Overijse et en ligne
+                            <h3 className='mt-6 md:mt-0 lg:mt-6 md:text-xs lg:text-sm lg:text-base'>
+                                Consultations à Overijse dans mon cabinet et en visioconférence
                             </h3>
-                            <div className='lg:flex hidden mt-6 justify-center'>
+                            <div className='lg:flex hidden mt-6 xl:mt-4 justify-center'>
                                 <GreenFullButton href='/doctoranytime' content="Prendre rendez-vous" />
                             </div>
                         </div>
